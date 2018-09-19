9653d1aa22ba9a1211975d597c5741f5d84b0b36
Prabhu Subramanian <subramanian.pr@husky.neu.edu>
2018-09-19 15:16:44 -0400

    Adding Lab 1.5 and Lab 2

---
+++ AED_Lab_1.5/src/Business/Person.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author Prabhu Surbamanian
+ */
+public class Person {
+    private String firstName = "Prabhu";
+    private String lastName = "Subramanian";
+    private String dob = "1993/09/09";
+    
+    Address workAddress;
+    Address homeAddress;
+    Address localAddress;
+   
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public Address getWorkAddress() {
+        return workAddress;
+    }
+
+    public void setWorkAddress(Address workAddress) {
+        this.workAddress = workAddress;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public Address getLocalAddress() {
+        return localAddress;
+    }
+
+    public void setLocalAddress(Address localAddress) {
+        this.localAddress = localAddress;
+    }
+    
+}
---
+++ LabPrep1/src/Business/Supplier.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author Prabhu Surbamanian
+ */
+public class Supplier {
+    
+    private String supplierName;
+    private String supplierRegNbr;
+    private String supplierDescription;
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getSupplierRegNbr() {
+        return supplierRegNbr;
+    }
+
+    public void setSupplierRegNbr(String supplierRegNbr) {
+        this.supplierRegNbr = supplierRegNbr;
+    }
+
+    public String getSupplierDescription() {
+        return supplierDescription;
+    }
+
+    public void setSupplierDescription(String supplierDescription) {
+        this.supplierDescription = supplierDescription;
+    }
+    
+    
+}
