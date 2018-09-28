d6382be73781ca33e359ed932ba6f6f2d025df79
Prabhu Subramanian <subramanian.pr@husky.neu.edu>
2018-09-28 13:50:33 -0400

    Addind Lab 2.5 to the repository

---
+++ Lab_2.5/src/Business/VitalSignHistory.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Prabhu Surbamanian
+ */
+public class VitalSignHistory {
+    private ArrayList<VitalSigns> vitalSignHistory;
+    
+    public VitalSignHistory(){
+        vitalSignHistory = new ArrayList<VitalSigns>();
+    }
+    
+    public VitalSigns addVital(){
+        VitalSigns vs = new VitalSigns();
+        vitalSignHistory.add(vs);
+        return vs;
+    }
+    
+    public void removeVital(VitalSigns v)
+    {
+        vitalSignHistory.remove(v);
+    }
+    
+    public ArrayList<VitalSigns> getVitalSignHistory() {
+        return vitalSignHistory;
+    }
+
+    public void setVitalSignHistory(ArrayList<VitalSigns> vitalSignHistory) {
+        this.vitalSignHistory = vitalSignHistory;
+    }
+
+    public List<VitalSigns> getAbnormalList(double maxbp, double minbp) {
+        List<VitalSigns> abnList = new ArrayList<>();
+        for(VitalSigns vs: vitalSignHistory){
+            if(vs.getBloodPressure()< maxbp || vs.getBloodPressure()> minbp){
+                abnList.add(vs);
+            }
+        }
+        return abnList;
+    }
+
+}
